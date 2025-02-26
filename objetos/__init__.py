8d4fafefd71c850cc9623e46df6a93d4c0c526c3
FerRomMu <fer.rom.mu@gmail.com>
2025-02-26 08:03:52 -0300

    Commit inicial.

---
+++ objetos/__init__.py
@@ -0,0 +1,4 @@
+from .query_result import QueryResult
+from .multiquery_result import MultiQueryResult
+
+__all__ = ['QueryResult', 'MultiqueryResult']
---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='objetos',
+    version='0.1',
+    packages=find_packages(),  # Encuentra automáticamente todos los subpaquetes
+    description='Some objects for educational purposes.',
+    author='fer.rom.mu',
+    author_email='fer.rom.mu.dev@gmail.com',
+    long_description_content_type='text/markdown',
+    url='https://github.com/FerRomMu/Objetos.git',
+    license='MIT'
+)
